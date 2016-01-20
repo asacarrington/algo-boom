77f9fa2c7e36d27c9efe7d582909103323e0be69
Carrington <acarrington@ukk82040.uk.kworld.kpmg.com>
2016-01-20 09:15:40 +0000

    converted form input

--- app/algorithm-selector/algorithm-data-form.component.ts
+++ app/algorithm-selector/algorithm-data-form.component.ts
@@ -9,6 +9,8 @@ import {AlgorithmProcessor} from "../algorithm-processor/AlgorithmProcessor";
 })
 export class AlgorithmDataFormComponent {
     public powers:Array<string> = ['quick', 'merge', 'bubble'];
+    public processedInput: Array<number>;   
+    public sortedOutput: Array<number>;
     public sortInput:string = "[ 1, 3, 5, 6, 7, 8, 9]";
     public algorithmService:AlgorithmProcessor;
     public model:AlgorithmModel = new AlgorithmModel("", "", "");
@@ -17,8 +19,31 @@ export class AlgorithmDataFormComponent {
     constructor(algorithmService:AlgorithmProcessor) {
         this.algorithmService = algorithmService;
     }
-
-    onSubmit() {
+    
+    convertInput = () => {
+        let temp: Array<string> = this.sortInput.split(',').splice(0,1).splice(this.sortInput.length, 1);
+        this.processedInput = new Array<number>();  
+         
+        for (var elm in temp)
+        {
+            this.processedInput.push(parseInt(elm));
+        }
+    } 
+    
+    onSubmit = () => {
+        this.convertInput();
+        switch(this.model.type){
+            case'merge':
+                this.sortedOutput = this.algorithmService.mergeSort(this.processedInput);
+            break;
+            case'quick':
+                 this.sortedOutput = this.algorithmService.quickSort(this.processedInput);
+            break;
+            case'bubble':
+                 this.sortedOutput = this.algorithmService.bubbleSort(this.processedInput);
+            break;
+            default:
+        } 
         this.submitted = true;
     }
 
